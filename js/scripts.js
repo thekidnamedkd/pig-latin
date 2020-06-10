3dddfe47c1374fe1f05afb26440356418fd4f276
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-10 14:25:24 -0700

    establish button functionality for the form. set input to lower case and scan input for i[0] of vowels - at this point that is 'a'.
    Co-authored-by: David Cole <davidtcole@yahoo.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-event.preventDefault();
-})
+  $("form#normInput").submit(function(event){
+    event.preventDefault();
+    const userInput = $("input#input").val();
+    pigLatin(userInput);
+  });   
+});
+
+function pigLatin(userInput) {
+  userInput = userInput.toLowerCase()
+  const vowels = ["a", "e", "i", "o", "u"];
+  console.log(userInput.includes(vowels[0]));
+  
+};
+
+
+  // if () {
+  //   alert("You haven't entered letters.");
+  // } 
